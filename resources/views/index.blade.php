034fae6fa6f0a5f09a66fbdb2bd86438dfc5c792
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2025-02-07 21:59:55 -0600

    Added logo and instagram

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,6 +16,16 @@
 </head>
 <body>
     <div class="my-5 w-full max-w-2xl mx-auto px-3">
+        <div class="flex justify-between">
+            <h1 class="font-mono text-center font-light text-zinc-600">jldstreet</h1>
+            <a href="https://www.instagram.com/jdlstreet" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+                    <path d="M21 3V21H3V3H21Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round" />
+                    <path d="M16.5 12C16.5 14.4853 14.4853 16.5 12 16.5C9.51472 16.5 7.5 14.4853 7.5 12C7.5 9.51472 9.51472 7.5 12 7.5C14.4853 7.5 16.5 9.51472 16.5 12Z" stroke="currentColor" stroke-width="1.5" />
+                    <path d="M17.5078 6.5L17.4988 6.5" stroke="currentColor" stroke-width="2" stroke-linecap="square" stroke-linejoin="round" />
+                </svg>
+            </a>
+        </div>
         <livewire:post.index />
     </div>
 </body>
